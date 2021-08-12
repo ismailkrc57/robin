af19e1047791882d587d9f8030ac6e1a83c5f2c2
ismailkrc57 <ismailkrc57@gmail.com>
2021-08-13 02:12:41 +0300

    default commit message

--- src/main/java/com/example/robin/api/controller/ProductController.java
+++ src/main/java/com/example/robin/api/controller/ProductController.java
@@ -1,12 +1,13 @@
 package com.example.robin.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("api//products")
 public class ProductController {
-    @RequestMapping("/getall")
+    @GetMapping("/getall")
     public String getAll()
     {
         return "Kanka burda kaldım merak etme gümbür gümbür gelecem";
