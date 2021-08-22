d65e1a54ba01319da0c2f3ed2efede590c736661
ismailkrc57 <ismailkrc57@gmail.com>
2021-08-22 23:43:52 +0300

    default commit message

--- src/main/java/com/example/robin/api/controller/ProductController.java
+++ src/main/java/com/example/robin/api/controller/ProductController.java
@@ -2,18 +2,21 @@ package com.example.robin.api.controller;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 @CrossOrigin
 @RestController("api//products")
 public class ProductController {
     @GetMapping("/getall")
-    public String getAll()
-    {
+    public String getAll() {
         return "Kanka burda kaldım merak etme gümbür gümbür gelecem";
     }
+
     @GetMapping("getone")
-    public String getOnes()
-    {
-        return   "KARACA";
+    public String getOnes(){
+        return "Karaca";
+    }
+
+    @PostMapping("/checkToken")
+    public boolean checkToken(@RequestParam String token) {
+        return true;
     }
 }
